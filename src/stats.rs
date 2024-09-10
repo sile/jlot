45799a9e7818c728b8e6a9163ac91651ef04aab3
Takeru Ohta <phjgt308@gmail.com>
2024-09-10 22:58:43 +0900

    [WIP] Add stats command

--- src/stats.rs
+++ src/stats.rs
@@ -5,6 +5,9 @@ use serde::Serialize;
 use crate::{io, stream_call::Output};
 
 /// Calculate statistics from JSON objects outputted by executing the command `stream-call --add-metadata ...`.
+///
+/// Note that the output of `stream-call` command does not include notifications,
+/// so the statistics do not take them into account.
 #[derive(Debug, clap::Args)]
 pub struct StatsCommand {}
 
@@ -22,10 +25,38 @@ impl StatsCommand {
 }
 
 #[derive(Debug, Default, Serialize)]
-struct Stats {}
+struct Stats {
+    output_count: usize,
+    non_metadata_output_count: usize,
+    batch_output_count: usize,
+
+    ok_response_count: usize,
+    error_response_count: usize,
+
+    min_latency: f64,
+    avg_latency: f64,
+    max_latency: f64,
+
+    incoming_bps: f64,
+    outgoing_bps: f64,
+
+    incoming_bytes: usize,
+    outgoing_bytes: usize,
+}
 
 impl Stats {
     fn handle_output(&mut self, output: Output) {
-        println!("{:?}", output);
+        self.output_count += 1;
+
+        // if matches!(output.response, ResponseObject::Ok { .. }) {
+        //     self.ok_count += 1;
+        // } else {
+        //     self.error_count += 1;
+        // }
+
+        let Some(metadata) = output.metadata else {
+            self.non_metadata_output_count += 1;
+            return;
+        };
     }
 }
